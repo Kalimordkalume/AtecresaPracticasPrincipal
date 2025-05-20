8c8d742115be6499fefd828eefd85edf613e0f57
Aytami91 <aytamihdez@gmail.com>
2025-05-20 11:59:28 +0100

    Puesto en metodo provisional index para consulta local

--- RestauranteDemoMVC/Controllers/MesaController.cs
+++ RestauranteDemoMVC/Controllers/MesaController.cs
@@ -13,10 +13,42 @@ namespace RestauranteDemoMVC.Controllers
             _coverManagerService = coverManagerService;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index2()
         {
             var mesas = await _coverManagerService.ObtenerMesasDisponiblesAsync("restaurante", "Fecha"); // Cambiar por datos reales
             return View(mesas);
+        }
+
+        public MesaController()
+        {
+            this.Mesas = TraerMesas();
+        }
+        public List<Mesa> Mesas { get; set; }
+
+        public List<Mesa> TraerMesas()
+        {
+            Mesa mesa = new Mesa();
+            mesa.MesaId = 1;
+            mesa.Capacidad = 4;
+            mesa.Ocupada = false;
+
+            Mesa mesa2 = new Mesa();
+
+            mesa2.MesaId = 2;
+            mesa2.Capacidad = 5;
+            mesa2.Ocupada = false;
+
+            List<Mesa> MesasNuevas = new List<Mesa>();
+
+            MesasNuevas.Add(mesa);
+            MesasNuevas.Add(mesa2);
+
+            return MesasNuevas;
+        }
+
+        public IActionResult Index()
+        {
+            return View(Mesas);
         }
 
     }
